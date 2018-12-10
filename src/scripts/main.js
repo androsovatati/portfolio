ea1a202f7687b8138e2e13541e76baebc3a5f837
Tatiana Androsova <androsovatati@mail.ru>
2018-12-10 17:22:43 +0300

    add navigation in left menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,28 +1,60 @@
 window.onload = () => {
-    const ACTIVE_MENU_ITEM = 'menu__item_active';
+    const ACTIVE_MENU_ITEM = "menu__item_active";
 
     let currentSection = 0;
-    const titleWrapper = document.getElementsByClassName('info__title')[0];
-    const menuItems = document.getElementsByClassName('menu__item');
-    const contentBlocks = document.getElementsByClassName('content-block');
+    const titleWrapper = document.getElementsByClassName("info__title")[0];
+    const menuItems = document.getElementsByClassName("menu__item");
+    const contentBlocks = document.getElementsByClassName("content-block");
+
+    let timer = null;
 
     setTimeout(() => {
-        titleWrapper.style.transition = 'transform .5s ease-in-out';
+        titleWrapper.style.transition = "transform .5s ease-in-out";
         titleWrapper.style.transform = `translateX(0)`;
     }, 500);
 
-    function changePage(e) {
-        const event = e || window.event;
-        const delta = e.deltaY || e.detail || e.wheelDelta;
+    function debounce(f, ms) {
+        let isCalling = false;
+        return (...args) => {
+            const onComplete = () => {
+                isCalling = false;
+            };
+            if (!isCalling) {
+                f.apply(this, args);
+                isCalling = true;
+                setTimeout(onComplete, ms);
+            }
+        };
+    }
 
-        if (delta > 0 && currentSection < 3) {
-            nextPage();
-        } else if (delta < 0 && currentSection > 0) {
+    function onMouseWheel(e) {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            const event = e || window.event;
+            const delta = e.deltaY || e.detail || e.wheelDelta;
+
+            if (delta > 0 && currentSection < 3) {
+                debounce(nextPage, 5000)();
+            } else if (delta < 0 && currentSection > 0) {
+                debounce(previousPage, 5000)();
+            }
+        }, 35);
+        
+    }
+
+    function onKeyDown({ keyCode }) {
+        if ((keyCode === 37 || keyCode === 38) && currentSection > 0) {
             previousPage();
+        } else if ((keyCode === 39 || keyCode === 40) && currentSection < 3) {
+            nextPage();
         }
     }
 
+    function changePage(i) {
+    }
+
     function nextPage() {
+        // debugger;
         menuItems[currentSection].classList.remove(ACTIVE_MENU_ITEM);
         currentSection++;
         titleWrapper.style.transform = `translateX(${-100 * currentSection}%)`;
@@ -46,7 +78,7 @@ window.onload = () => {
         currentSection--;
         titleWrapper.style.transform = `translateX(${-100 * currentSection}%)`;
         for (let i = currentSection + 1; i <= 3; i++) {
-            contentBlocks[i].style.top = '100%';
+            contentBlocks[i].style.top = "100%";
         }
         for (let i = 0; i <= currentSection; i++) {
             contentBlocks[i].style.opacity = 1;
@@ -54,19 +86,60 @@ window.onload = () => {
         menuItems[currentSection].classList.add(ACTIVE_MENU_ITEM);
     }
 
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener('click', () => {
+            menuItems[currentSection].classList.remove(ACTIVE_MENU_ITEM);
+            const isNext = currentSection < i;
+            currentSection = i;
+            if (isNext) {
+                titleWrapper.style.transform = `translateX(${-100 * currentSection}%)`;
+                for (let i = 1; i <= currentSection; i++) {
+                    contentBlocks[i].style.top = 0;
+                }
+                if (currentSection === 3) {
+                    for (let i = 0; i < currentSection; i++) {
+                        contentBlocks[i].style.opacity = 0;
+                    }
+                } else {
+                    for (let i = 0; i < currentSection; i++) {
+                        contentBlocks[i].style.opacity = 1;
+                    }
+                }
+            } else {
+                titleWrapper.style.transform = `translateX(${-100 * currentSection}%)`;
+                for (let i = currentSection + 1; i <= 3; i++) {
+                    contentBlocks[i].style.top = "100%";
+                }
+                for (let i = 0; i <= currentSection; i++) {
+                    contentBlocks[i].style.opacity = 1;
+                }
+            }
+            menuItems[i].classList.add(ACTIVE_MENU_ITEM);
+        });
+    }
+
     if (document.addEventListener) {
-        if ('onwheel' in document) {
+        if ("onwheel" in document) {
             // IE9+, FF17+, Ch31+
-            document.addEventListener('wheel', changePage);
-        } else if ('onmousewheel' in document) {
+            document.addEventListener("wheel", onMouseWheel);
+        } else if ("onmousewheel" in document) {
             // устаревший вариант события
-            document.addEventListener('mousewheel', changePage);
+            document.addEventListener("mousewheel", onMouseWheel);
         } else {
             // Firefox < 17
-            document.addEventListener('MozMousePixelScroll', changePage);
+            document.addEventListener("MozMousePixelScroll", onMouseWheel);
         }
+
+        document.addEventListener("keydown", onKeyDown);
+
+        // for (let i = 0; i < menuItems.length; i++) {
+        //     menuItems[i].addEventListener('click', () => {
+        //         menuItems[currentSection].classList.remove(ACTIVE_MENU_ITEM);
+        //         console.log(i);
+        //     });
+        // }
     } else {
         // IE8-
-        document.attachEvent('onmousewheel', changePage);
+        document.attachEvent("onmousewheel", onMouseWheel);
     }
 };
